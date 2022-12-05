dfa961f11170d86920f32b966d01454b824c444e
kirshr <astudillokiran@gmail.com>
2022-12-05 09:42:16 -0700

    Nav Icons and form

--- components/navigation/NavBar.jsx
+++ components/navigation/NavBar.jsx
@@ -1,13 +1,28 @@
 import MobileMenuButton from "./MobileMenuButton";
 import NavBarBranding from "./NavBarBranding";
 import NavBarLinks from "./NavBarLinks";
-import LoginButton from "./LoginButton";
+import GitHubLink from "./GitHubLink";
+import DeploymentLink from "./DeploymentLink";
+import LoginLink from "./LoginLink";
 function NavBar() {
   return (
     <nav className="bg-gradient-to-r from-gray-700 via-gray-900 to-black border-b-2 px-2 py-2 flex justify-between shadow items-center">
       <NavBarBranding />
       <NavBarLinks />
-      <LoginButton className="text-amber-300" />
+      <div className="hidden md:flex gap-8 justify-center align-center">
+        <figure className="flex flex-col items-center  ">
+          <LoginLink />
+          <figcaption className="text-[11px] text-cyan-50">Sign up</figcaption>
+        </figure>
+        <figure className="flex flex-col items-center">
+          <DeploymentLink />
+          <figcaption className="text-[11px] text-green-600">Deploy</figcaption>
+        </figure>
+        <figure className="flex flex-col items-center">
+          <GitHubLink />
+          <figcaption className="text-[11px] text-blue-600">GitHub</figcaption>
+        </figure>
+      </div>
       <MobileMenuButton />
     </nav>
   );
